c1b59184d4a087c3c79c5dfd1aadf8c2409c14e0
= <=>
2023-05-23 22:38:01 -0500

    initial commit

---
+++ api/autoridades.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$autoridades = [
+    [
+        "id" => 1,
+        "nombre_autoridad" => "Mg. Victor Raúl Rodriguez Peña",
+        "cargo_autoridad" => "Director de la E. P. de Ingeniería de Sistemas",
+        "foto_autoridad" => "iamgen001.jpg"
+    ]
+];
+
+
+echo json_encode($autoridades);
+
+
+?> 
