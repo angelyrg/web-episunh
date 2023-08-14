caafee9f1a309dea1a7d3a4ab222ae15d5385437
Angel Yaranga <angel.yaranga@bitel.com.pe>
2023-08-14 16:11:40 -0500

    feat: agrega API para obtener la info de autoridades

--- api/autoridades.php
+++ api/autoridades.php
@@ -1,17 +1,43 @@
 <?php
+try {
+    require("../model/Authority.php");
+    $autoridades = new Authority();
+    $all_data = $autoridades->get_all();
+    $cantidad = count($all_data);
 
+} catch (Exception $e) {
+    $response = [
+        "code" => "500",
+        "message" => "Error al obtener los datos",
+        "data" => []
+    ];
 
-$autoridades = [
-    [
-        "id" => 1,
-        "nombre_autoridad" => "Mg. Victor Raúl Rodriguez Peña",
-        "cargo_autoridad" => "Director de la E. P. de Ingeniería de Sistemas",
-        "foto_autoridad" => "iamgen001.jpg"
-    ]
-];
+    header("Content-Type: application/json");
+    echo json_encode($response);
+    exit();
+}
 
+$data_response = [];
+foreach ($all_data as $value) {
+    $temp = [
+        'id' => $value['id'],
+        'nombres' => $value['name'],
+        'apellidos' => $value['lastname'],
+        'grado' => $value['degree'],
+        'nombre_completo' => $value['degree']." ".$value['name']." ".$value['lastname'],
+        'cargo' => $value['position'],
+        'url_foto' => $_SERVER['HTTP_HOST']."/upload/images/".$value['photo'],
+    ];
+    array_push($data_response, $temp);
+}
 
-echo json_encode($autoridades);
+$response = array(
+    'code' => '200',
+    'message' => 'Ok',
+    'data' => $data_response,
+);
 
+header("Content-Type: application/json");
+echo json_encode($response);
 
 ?> 
