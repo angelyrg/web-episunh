4fe33d42fdbbb9fbc705a73ef52e95da4284fce8
Jasmina-Y <llacuagrayce@gmail.com>
2023-08-28 23:27:40 -0500

    modificacion

--- model/config.php
+++ model/config.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_HOST','localhost');
-    define('DB_USERNAME','root');
-    define('DB_PASS','');
+    define('DB_USERNAME','Jasmina');
+    define('DB_PASS','12345');
     define('DB_NAME','web_sistemasunh');
     // define('DB_CHARSET','UTF-8');
 ?>
